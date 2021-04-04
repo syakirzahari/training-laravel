ad2ed0fdcdf81143cc9ea950096b89d9f0a9cc86
Syakir  Zahari <syakirzahari97@gmail.com>
2021-04-04 08:47:24 +0800

    update middleware

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -10,6 +10,11 @@ use DB;
 
 class StudentController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     //
     public function index(){
 
--- app/Http/Controllers/TimetableController.php
+++ app/Http/Controllers/TimetableController.php
@@ -11,6 +11,11 @@ use Illuminate\Http\Request;
 
 class TimetableController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
